bba5a8ca9ae5951e6be580bae41dfe712cd904d1
KimCh-desktop <akswl1002@gmail.com>
2024-03-22 05:53:28 +0900

    04.Nuxt 주요 개념 및 특징
    nuxtApp (Runtime App Context)
    
    아직뭔지 잘 모르겠지만....실제로 개발을 한다면 많이 사용할것 같은 기능이다.
    
    types, plugins에 추가한 파일을 중점적으로 확인해볼 필요가 있다.

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+// const nuxtApp = useNuxtApp();
+// const vueApp = nuxtApp.vueApp;
+
+// console.log(nuxtApp);
+// console.log(vueApp);
+
+// nuxtApp.provide('hello', (msg: string) => `Hello ${msg}`);
+</script>
