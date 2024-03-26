ba633792e49bd6c5809f0c8aaa3f38ea8f141683
KimCh-desktop <akswl1002@gmail.com>
2024-03-26 17:10:43 +0900

    06. Route Middleware and Authentication
    Authentication 비즈니스 로직 구현
    
    userAuthUser.ts에 authUser임시로 전역처리함

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,7 +40,12 @@
           </q-list>
         </q-btn-dropdown>
         <q-separator dark vertical />
-        <NuxtLink v-slot="{ navigate }" custom to="/login">
+        <NuxtLink
+          v-if="!isAuthenticated"
+          v-slot="{ navigate }"
+          custom
+          to="/login"
+        >
           <q-btn
             stretch
             flat
@@ -49,23 +54,37 @@
             @click="navigate()"
           />
         </NuxtLink>
-        <NuxtLink v-slot="{ navigate }" custom to="/">
+        <!-- <NuxtLink v-else v-slot="{ navigate }" custom to="/">
           <q-btn
             stretch
             flat
             :label="$t('logout')"
             no-caps
-            @click="navigate()"
+            @click="signOut()"
           />
-        </NuxtLink>
+        </NuxtLink> -->
+        <q-btn
+          v-else
+          stretch
+          flat
+          :label="$t('logout')"
+          no-caps
+          @click="signOut()"
+        />
       </q-toolbar>
     </q-header>
     <q-page-container :style="pageContainerStyle">
+      <q-banner v-if="isAuthenticated" class="bg-primary text-white">
+        {{ authUser }}
+      </q-banner>
       <slot></slot>
     </q-page-container>
   </q-layout>
 </template>
 <script setup lang="ts">
+const { authUser, isAuthenticated } = useAuthUser();
+const { signOut } = useAuth();
+
 const pageContainerStyle = computed(() => ({
   maxWidth: '1080px',
   margin: '0 auto',
