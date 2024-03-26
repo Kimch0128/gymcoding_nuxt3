fff70342fe54aa946d35a8ef17868ff68eb1ff88
KimCh-desktop <akswl1002@gmail.com>
2024-03-26 15:07:11 +0900

    05.Error Handling
    Error Utils - showError
    
    전체화면 에러를 빠르게 표시
    app:error 훅 호출
    클라이언트에서 직접 호출, 미들웨어, 플러그인, setup() 에서 사용

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -10,4 +10,13 @@ export default defineNuxtPlugin((nuxtApp) => {
       });
     }
   });
+  nuxtApp.hook('app:error', (err) => {
+    // console.log('vue:error', err);
+    if (err instanceof Error) {
+      Notify.create({
+        message: err.message,
+        type: 'warning',
+      });
+    }
+  });
 });
