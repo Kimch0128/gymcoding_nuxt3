3740a935e902aa87dbdb6d79df2fedca2f891266
KimCh-desktop <akswl1002@gmail.com>
2024-04-02 18:16:46 +0900

    08.Server & Data fetching
    Server Auth Utils & Middleware
    
    미들웨어는 가장 먼저 수행
    로그, 인증 확인등에 사용하기 적합

--- server/routes/auth/user.get.ts
+++ server/routes/auth/user.get.ts
@@ -1,14 +1,23 @@
 export default defineEventHandler((event) => {
-  const userJsonString = getCookie(event, '__user');
+  // 미들웨어에서 가져옴
+  const user = event.context.user;
+  // const user = getUserFromEvent(event);
 
-  console.log('userJsonString', userJsonString);
-
-  if (!userJsonString) {
+  if (!user) {
     return { user: null };
   }
 
-  const user = JSON.parse(userJsonString);
-  return {
-    user,
-  };
+  return { user };
+  // const userJsonString = getCookie(event, '__user');
+
+  // console.log('userJsonString', userJsonString);
+
+  // if (!userJsonString) {
+  //   return { user: null };
+  // }
+
+  // const user = JSON.parse(userJsonString);
+  // return {
+  //   user,
+  // };
 });
